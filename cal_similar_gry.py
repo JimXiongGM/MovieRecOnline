384e6e165e12dba785c577a154669e80f014adf1
unknown <gm_xiong@qq.com>
2019-05-15 09:08:35 +0800

    更新格式

--- cal_similar_gry.py
+++ cal_similar_gry.py
@@ -1,16 +1,19 @@
 '''
 @author : Gong RY
 '''
-import MySQLdb,distance
+import MySQLdb
+import distance
+
 
 def run_cal():
-    db = MySQLdb.connect(host="localhost", port=3308, user="root", passwd="你的密码", db="MovieSizer", charset="utf8")
+    db = MySQLdb.connect(host="localhost", port=3308, user="root",
+                         passwd="你的密码", db="MovieSizer", charset="utf8")
     cursor = db.cursor()
     sql = "select * from movies_movieinfo"
     cursor.execute(sql)
     res = cursor.fetchall()
     id = 1
-    print ('\n\n cal_similar_gry.py\n\n')
+    print('\n\n cal_similar_gry.py\n\n')
     # 控制矩阵规模
     for i in range(0, int(len(res)/100)):
         for j in range(i+1, len(res)):
@@ -21,15 +24,18 @@ def run_cal():
             directors_length = distance.levenshtein(res[i][4], res[j][4])
             leadactors_length = distance.levenshtein(res[i][5], res[j][5])
             editors_length = distance.levenshtein(res[i][6], res[j][6])
-            length = moviename_length + nation_length + directors_length + leadactors_length + editors_length
+            length = moviename_length + nation_length + \
+                directors_length + leadactors_length + editors_length
             similar = 1/length
-            sql = "INSERT INTO movies_moviesimilar VALUES (%d, %d, %d, %f)" % (id, i_id, j_id, similar)
+            sql = "INSERT INTO movies_moviesimilar VALUES (%d, %d, %d, %f)" % (
+                id, i_id, j_id, similar)
             cursor.execute(sql)
             db.commit()
-            id = id +1
-        print('current : ',i)
+            id = id + 1
+        print('current : ', i)
     db.close()
-    print ('DONE !')
-    
+    print('DONE !')
+
+
 if __name__ == '__main__':
     run_cal()
